eb45f27f89eb04e51845b9e5026c9a136c4d76ec
Diegoo11 <ynoacamino@gmail.com>
2023-03-31 16:17:45 -0500

    xd

---
+++ prueba.py
@@ -0,0 +1,18 @@
+import turtle
+#Procedimiento para crear el cuadrado
+windows = turtle.Screen()
+windows.bgcolor('yellow')
+t1 = turtle.Turtle()
+#t1.penup()
+t1.goto(200, 200)
+t1.left(90)
+t1.forward(100)
+t1.circle(50)
+
+t2 = turtle.Turtle()
+t2.penup()
+t2.goto(-200, 200)
+t2.pendown()
+t2.write("BINGO", move=False, font=('Arial', 24, "bold"))
+
+turtle.done()
