ff40476fdc0fc74fd164a5cbd2ea0255e52d9a03
Diegoo11 <ynoacamino@gmail.com>
2023-03-31 16:17:51 -0500

    xd

--- prueba.py
+++ prueba.py
@@ -2,6 +2,7 @@ import turtle
 #Procedimiento para crear el cuadrado
 windows = turtle.Screen()
 windows.bgcolor('yellow')
+
 t1 = turtle.Turtle()
 #t1.penup()
 t1.goto(200, 200)
@@ -14,5 +15,14 @@ t2.penup()
 t2.goto(-200, 200)
 t2.pendown()
 t2.write("BINGO", move=False, font=('Arial', 24, "bold"))
+
+pen3 = turtle.Turtle()
+pen3.fillcolor((0, 0, 0))
+pen3.begin_fill()
+pen3.circle(100)
+pen3.end_fill()
+
+
+
 
 turtle.done()
